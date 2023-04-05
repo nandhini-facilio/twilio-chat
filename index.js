70699871e4ffe261b055954d78c6efd5a59cd135
nandhini-facilio <107096781+nandhini-facilio@users.noreply.github.com>
2023-04-05 12:21:40 +0530

    changes

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const { urlencoded } = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(urlencoded({ extended: false }));
+app.use(cors());
+
+const port = 3000;
+
+const AccessToken = require("twilio").jwt.AccessToken;
+const ChatGrant = AccessToken.ChatGrant;
+
+// Used when generating any kind of tokens
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKey = process.env.TWILIO_API_KEY_SID;
+const twilioApiSecret = process.env.TWILIO_API_KEY_SECRET;
+
+// Used specifically for creating Chat tokens
+const serviceSid = process.env.TWILIO_CONVERSATIONS_SERVICE_SID;
+
+// Create a "grant" which enables a client to use Chat as a given user,
+// on a given device
+const chatGrant = new ChatGrant({
+  serviceSid: serviceSid,
+});
+
+// Create an access token which we will sign and return to the client,
+// containing the grant we just created
+function getAccessToken(user) {
+  const token = new AccessToken(
+    twilioAccountSid,
+    twilioApiKey,
+    twilioApiSecret,
+    { identity: user }
+  );
+  token.addGrant(chatGrant);
+  const jwt = token.toJwt();
+  console.log(`Token for ${user}: ${jwt}`);
+  return jwt;
+}
+
+app.get("/auth/user/:user", (req, res) => {
+  const jwt = getAccessToken(req.params.user);
+  res.send({ token: jwt });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ public/chat.js
@@ -0,0 +1,104 @@
+/* eslint-disable semi */
+/* eslint-disable indent */
+(function() {
+    new Vue({
+      el: "#app",
+      data: {
+        statusString: "",
+        activeConversation: null,
+        username: "",
+        isConnected: false,
+        messages: [],
+        messageText: "",
+        isSignedInUser: false,
+      },
+  
+      // Initialize after component creation
+      created: function() {
+        window.facilioApp = FacilioAppSDK.init();
+        window.facilioApp.on("app.loaded", (data) => {
+          this.username = window.facilioApp.getCurrentUser().email;
+          this.registerUser();
+        });
+      },
+  
+      computed: {},
+  
+      methods: {
+        initConversationsClient: async function() {
+          const token = await this.getToken(this.username);
+          this.conversationsClient = new Twilio.Conversations.Client(token);
+          this.statusString = "Connecting to Twilio…";
+          this.conversationsClient.on("connectionStateChanged", (state) => {
+            switch (state) {
+              case "connected":
+                this.statusString = "You are connected.";
+                this.isConnected = true;
+                break;
+              case "disconnecting":
+                this.statusString = "Disconnecting from Twilio…";
+                break;
+              case "disconnected":
+                this.statusString = "Disconnected.";
+                break;
+              case "denied":
+                this.statusString = "Failed to connect.";
+                break;
+            }
+          });
+        },
+        getToken: async function(identity) {
+          const response = await fetch(`/auth/user/${identity}`);
+          const responseJson = await response.json();
+          return responseJson.token;
+        },
+        registerUser: async function() {
+          await this.initConversationsClient();
+        },
+        createConversation: async function() {
+          // Ensure User1 and User2 have an open client session
+          try {
+            await this.conversationsClient.getUser("User1");
+            await this.conversationsClient.getUser("User2");
+          } catch {
+            console.error("Waiting for User1 and User2 client sessions");
+            return;
+          }
+          // Try to create a new conversation and add User1 and User2
+          // If it already exists, join instead
+          try {
+            const newConversation = await this.conversationsClient.createConversation(
+              { uniqueName: "chat" }
+            );
+            const joinedConversation = await newConversation
+              .join()
+              .catch((err) => console.log(err));
+            await joinedConversation
+              .add("User1")
+              .catch((err) => console.log("error: ", err));
+            await joinedConversation
+              .add("User2")
+              .catch((err) => console.log("error: ", err));
+            this.activeConversation = joinedConversation;
+          } catch {
+            this.activeConversation = await this.conversationsClient.getConversationByUniqueName(
+              "chat"
+            );
+          }
+  
+          this.activeConversation.getMessages().then((newMessages) => {
+            this.messages = [...this.messages, ...newMessages.items];
+          });
+          this.activeConversation.on("messageAdded", (message) => {
+            this.messages = [...this.messages, message];
+          });
+        },
+        sendMessage: function() {
+          this.activeConversation.sendMessage(this.messageText).then(() => {
+            this.messageText = "";
+          });
+        },
+      },
+    });
+  })();
+  
